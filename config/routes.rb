86dac4f9ee215edd498a1add3dc98f886cb15294
tuckseng <tuckywong@live.com>
2024-01-08 15:30:51 +0800

    added articles listing feature

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "pages#home"
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show]
+  resources :articles, only: [:show,:index]
 
 end
