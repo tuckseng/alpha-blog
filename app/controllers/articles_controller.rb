a9b07a762345b2894bfd0cc1e3b92948f132ffdf
tuckseng <tuckywong@live.com>
2024-01-08 19:32:26 +0800

    ready to deploy to prod

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
-    def show
-        @article = Article.find(params[:id])
+before_action:set_article, only: [:show, :edit, :update, :destroy]
+
+    def show 
     end
 
     def index
@@ -12,13 +13,12 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
     def create
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
         if @article.save
-            flash[:notice] = "Article was created successfully"
+            flash[:notice] = "Article was created successfully."
             redirect_to @article
         else
             render "new"
@@ -26,13 +26,27 @@ class ArticlesController < ApplicationController
     end
 
     def update
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description))
-            flash[:notice] = "Article was updated successfully"
+        if @article.update(article_params)
+            flash[:notice] = "Article was updated successfully."
             redirect_to @article
         else
-            render "edit"
+            render :edit, status: :unprocessable_entity
         end
+    end
+
+    def destroy
+        @article.destroy
+        redirect_to @article
+    end
+
+    private
+
+    def set_article
+    @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
     end
 
 end
