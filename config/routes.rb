e5cb0d3cc200a20e7d9d09e707e0c88a55fcd7c7
tuckseng <tuckywong@live.com>
2024-01-08 17:28:28 +0800

    added edit feature

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "pages#home"
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show,:index]
+  resources :articles, only: [:show,:index, :new, :create, :edit, :update]
 
 end
