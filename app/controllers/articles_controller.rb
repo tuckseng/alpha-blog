ffc3857f3f8b3e966328539c2e99c827e0d35979
tuckseng <tuckywong@live.com>
2024-01-09 23:29:22 +0800

    Add one-to-many association between users and articles from back end

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,11 +17,12 @@ before_action:set_article, only: [:show, :edit, :update, :destroy]
 
     def create
         @article = Article.new(article_params)
+        @article.user = User.first
         if @article.save
             flash[:notice] = "Article was created successfully."
             redirect_to @article
         else
-            render "new"
+            render :new, status: :unprocessable_entity
         end
     end
 
